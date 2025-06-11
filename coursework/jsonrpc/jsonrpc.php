72a5f7e049ca25fd0471a0254706716c0523e891
Jack Jefferson <dev.jac.jef@proton.me>
2025-06-11 15:29:54 +0100

    Finally got the connection working.
    Just need bugfixes/testing for the entire API.
    Minor refactors are needed in some spots.

--- coursework/jsonrpc/jsonrpc.php
+++ coursework/jsonrpc/jsonrpc.php
@@ -3,11 +3,13 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-include 'deps/client.php';
+require_once 'client.php';
 require_once 'deps/request.php';
 
-$thinkpad = new Thinkpad("22", "Thinkpad T41", "Beefy", "./image03.jpg");
-$request = new RpcRequest("createThinkpad", $thinkpad, 510572);
+// $thinkpad = new Thinkpad("22", "Thinkpad T41", "Beefy", "./image03.jpg");
+// $request = new RpcRequest("createThinkpad", $thinkpad, 510572);
+
+// TODO Need to test all functionality.
 
 $client = new RpcClient($request, "http://localhost:8080/index.php");
 // Forgot to echo the output cause im a numpty originally...
