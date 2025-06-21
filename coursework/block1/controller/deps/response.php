aabcbfc402c534adb40f5cc69c220340f7e95062
Jack Jefferson <dev.jac.jef@proton.me>
2025-06-21 20:11:17 +0100

    backing up work, fedora is a slow distro

--- coursework/block1/controller/deps/response.php
+++ coursework/block1/controller/deps/response.php
@@ -3,34 +3,40 @@
 require "error.php";
 
 // Response gets sent from the server to the client
- 
+
 class RpcResponse implements JsonSerializable
 {
-    private $jsonrpc;
-    private $result; // this is the json rpc as text
-    private $error; // this is going to store an assoc array
-    private $id;
+   private $jsonrpc;
+   private $result; // this is the json rpc as text
+   private $error; // this is going to store an assoc array
+   private $id;
 
-    public function __construct($result = null,  $error = null, $id)
-    {
-        $this->jsonrpc = "2.0";
-        $this->result = $result;
-        $this->error = $error;
-        $this->id = $id;
-    }
+   public function __construct($result = null,  $error = null, $id)
+   {
+      $this->jsonrpc = "2.0";
+      $this->result = $result;
+      $this->error = $error;
+      $this->id = $id;
+   }
 
-    public function jsonSerialize(): mixed
-    {
-        return [
-            'jsonrpc' => $this->jsonrpc,
-            'result' => $this->result,
-            'error' => $this->error,
-            'id' => $this->id
-        ];
-    }
+   public function jsonSerialize(): mixed
+   {
+      return [
+         'jsonrpc' => $this->jsonrpc,
+         'result' => $this->result,
+         'error' => $this->error,
+         'id' => $this->id
+      ];
+   }
 
-    public function toJson()
-    {
-        return json_encode($this);
-    }
+   public function toJson()
+   {
+      return json_encode($this);
+   }
+
+   // A shortcut if you will... TODO: Maybe tidy this up later
+   public function resultJson()
+   {
+      return json_encode($this->result);
+   }
 }
