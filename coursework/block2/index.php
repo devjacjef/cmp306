c3cbbe9a016838e035e30b646028c8b62715b1cb
devjacjef <dev.jac.jef@proton.me>
2025-07-15 02:12:08 +0100

    Organised some stuff

---
+++ coursework/block2/index.php
@@ -0,0 +1,6 @@
+<?php
+
+session_start();
+
+header("Location: /coursework/block2/views/index.php");
+exit;
--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-header('Location: /cmp306/coursework/index.php');
+header('Location: /~2207061/cmp306/coursework/index.php');
 exit;
